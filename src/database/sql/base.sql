bcd3c4c97ae364ab87ba04c9e0d6dd83912b4bde
Seabath <sebastien.fontanel@hotmail.fr>
2017-02-11 15:28:32 +0100

    nik je passe sur linux

--- src/database/sql/base.sql
+++ src/database/sql/base.sql
@@ -1,7 +1,24 @@
-CREATE TABLE 'USER' (
-  id int NOT NULL AUTO_INCREMENT,
-  'login' VARCHAR(20) UNIQUE NOT NULL,
-  'password' VARCHAR(20) NOT NULL,
+CREATE DATABASE JustJava
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_general_ci;
 
+CREATE USER 'java'@'localhost'
+  IDENTIFIED BY 'java';
+GRANT ALL ON JustJava.* TO 'java'@'localhost'
+IDENTIFIED BY 'java';
+
+CREATE TABLE USER (
+  id       INT                NOT NULL AUTO_INCREMENT,
+  login    VARCHAR(20) UNIQUE NOT NULL,
+  password VARCHAR(20)        NOT NULL,
   PRIMARY KEY (id)
 );
+
+DROP FUNCTION IF EXISTS insert_user;
+CREATE FUNCTION insert_user(p_login VARCHAR(20), p_password VARCHAR(20))
+  RETURNS INT
+  BEGIN
+    INSERT INTO USER (login, PASSWORD)
+    VALUES (p_login, p_password);
+    RETURN LAST_INSERT_ID();
+  END;
